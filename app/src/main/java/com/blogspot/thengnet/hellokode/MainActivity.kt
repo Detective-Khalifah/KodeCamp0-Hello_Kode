1d6dd5039b0cc0fd2228297d45f9985aa6ea7484
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-08-27 23:55:25 +0100

    Display image when button toggled Set background colour on parent view group {@link View}, {@link ConstraintLayout} when button toggled

--- app/src/main/java/com/blogspot/thengnet/hellokode/MainActivity.kt
+++ app/src/main/java/com/blogspot/thengnet/hellokode/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.blogspot.thengnet.hellokode
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val parentViewGrp: ConstraintLayout = findViewById(R.id.constraint)
+
         val greetButton: Button = findViewById(R.id.sayGreeting)
         val birthdayGreeting: TextView = findViewById(R.id.bGreeting)
+        val birthdayCard: ImageView = findViewById(R.id.birthdayCard)
         greetButton.setOnClickListener {
             birthdayGreeting.text = "Happy Birthday!"
             birthdayGreeting.visibility = View.VISIBLE
+
+            birthdayCard.visibility = View.VISIBLE
+
+            parentViewGrp.setBackgroundColor(Color.YELLOW)
         }
     }
 
