dd92c162d46e4e899884172e486c174e60a9ca24
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-08-27 23:46:31 +0100

    Use event-handling to say greeting when button is clicked/tapped

--- app/src/main/java/com/blogspot/thengnet/hellokode/MainActivity.kt
+++ app/src/main/java/com/blogspot/thengnet/hellokode/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.blogspot.thengnet.hellokode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greetButton: Button = findViewById(R.id.sayGreeting)
+        val birthdayGreeting: TextView = findViewById(R.id.bGreeting)
+        greetButton.setOnClickListener {
+            birthdayGreeting.text = "Happy Birthday!"
+            birthdayGreeting.visibility = View.VISIBLE
+        }
     }
+
 }
